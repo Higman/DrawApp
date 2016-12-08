6cb38bb469c4c530b914f4ed46f7f09395040b10
asanoyu <s15t201@stu.kagawa-u.ac.jp>
2016-12-08 21:29:41 +0900

    create

---
+++ app/src/main/java/com/asanoyu/drawapp/MainActivity.java
@@ -0,0 +1,27 @@
+package com.asanoyu.drawapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final CanvasView cv = (CanvasView) findViewById(R.id.canvas_view);
+
+        //-- Button関連
+        Button bt = (Button) findViewById(R.id.clear_button);
+        //- 動作設定
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cv.allDelete();
+            }
+        });
+    }
+}
